2f8bb895c6ffd7810ba23e2e66214fcbc061895e
= <=icaua12@gmail.com>
2022-11-08 21:50:50 -0300

    fundamentos da linguagem

--- arrayMultidimensional.js
+++ arrayMultidimensional.js
@@ -10,3 +10,5 @@ let boletim = [[8,7,9,3],[4,5,8,6]]
 //recuperando a nota de matematica do segundo bimestre
 //primeiro é [linha] e segundo é [coluna]
 console.log(`nota: ${boletim[0][1]}` )
+//
+console.log(`nota: ${boletim[1][2]}` )
