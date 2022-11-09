155e1bb59722989f016032d469c86763daab8ba7
= <=icaua12@gmail.com>
2022-11-08 21:47:59 -0300

    fundamentos da linguagem

---
+++ arrayMultidimensional.js
@@ -0,0 +1,12 @@
+/**
+ * Estudo do array multidimensional 
+ */
+
+//[]linha 1
+//[]linha 2
+//              mat.       port.
+let boletim = [[8,7,9,3],[4,5,8,6]]
+//recuperando um valor da matriz (ex : boletim escolar)
+//recuperando a nota de matematica do segundo bimestre
+//primeiro é [linha] e segundo é [coluna]
+console.log(`nota: ${boletim[0][1]}` )
