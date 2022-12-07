3437494503e11935522f4cadcb78ee7bf4ec851b
= <=icaua12@gmail.com>
2022-12-07 19:30:22 -0300

    atividade sirlene

--- atividade/atividade.js
+++ atividade/atividade.js
@@ -3,7 +3,7 @@ const read = require('readline-sync')
 
 let produto = Number(read.question("valor em R$"))
 let conversão = produto / 5.3600
-console.log(`valor em dolar R$${conversão.toFixed(2)}`)
+console.log(`valor em dolar $${conversão.toFixed(2)}`)
 
 let imposto = (conversão * 0.3) + conversão
 console.log(`valor com imposto R$${imposto.toFixed(2)}`)
